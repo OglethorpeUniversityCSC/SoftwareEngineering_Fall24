ef0a9816c53b4b627b55b054f2668039f7795568
Brian Patterson <bpatterson@oglethorpe.edu>
2024-09-09 15:31:09 -0400

    Note this

--- GitHubCheck.py
+++ GitHubCheck.py
@@ -1,6 +1,6 @@
 #Add your own version of hello world below to show you have access to this repository
 
-print("Hello, Dr. Patterson figured it out!  Again!")
+print("Hello, Dr. Patterson figured it out!")
 
 #print("Hello, Saif Farooqi figured it out too.")
 
@@ -11,9 +11,6 @@ print("Hello, Dr. Patterson figured it out!  Again!")
 #print("Hello, Noah figured it out too.")
 
 #print("Hello, Jose figured it out too.")
-
-#while True:
-  #print("I have no clue what I am doing")
 
 #print("Hello, Sofia figured it out too.")
 
